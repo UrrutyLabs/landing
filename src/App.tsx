1effc201d5ae0aab625b0b0c89030b6cb6d487eb
Nicolas Urruty <nicolas.u@leanzupply.com>
2026-01-06 16:07:12 -0300

    add seo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -7,19 +8,21 @@ import LeanZupplyCaseStudy from "./pages/case-studies/LeanZupplyCaseStudy";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/case-studies" element={<CaseStudies />} />
-          <Route
-            path="/case-studies/leanzupply"
-            element={<LeanZupplyCaseStudy />}
-          />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/case-studies" element={<CaseStudies />} />
+            <Route
+              path="/case-studies/leanzupply"
+              element={<LeanZupplyCaseStudy />}
+            />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
